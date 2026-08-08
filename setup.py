a99f92910c6c0fa8d1806c9263fa267e01c8d341
Rohit Kumar <rkrohit2059@gmail.com>
2026-08-08 13:22:28 +0530

    first commit

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
 name = "mlproject",
 version = "0.0.1",
 author = "Rohit",
-author_email = "rkrohit2059@gmail.com",
+author_email = "rkrohit2059@gmail.com" ,
 packages = find_packages(),
 install_requires = get_requirements("requirements.txt")
 )
